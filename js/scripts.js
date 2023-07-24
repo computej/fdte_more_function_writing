5cf9d73feafa188246e6c4d858dba101aee9d5fa
computej <computejpdc@gmail.com>
2023-07-24 09:57:40 -0700

    add function for converting gallons to litres

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,19 @@ function celsiusToFahrenheit(tempoCelsius) {
   tempoFahrenheit += 32;
   return tempoFahrenheit;
 }
+
+function usGallonsUsToLitres(volGallons) {
+
+  /*  
+  * "The US liquid gallon 
+  * (frequently called simply "gallon") is legally defined
+  * as 231 cubic inches, which is exactly 
+  * 3.785411784 litres"
+  * https://en.wikipedia.org/wiki/Gallon
+  * 
+  * knowing this, it should be easy to convert, just multiply by 3.78..
+  */
+  const litresInGallon = 3.785411784;
+  let volLitres = volGallons * litresInGallon;
+  return volLitres;
+}
